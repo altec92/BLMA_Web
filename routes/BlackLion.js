efe995d479646dd6528b60336a52d22eeb85c07f
altec92 <alexander.forward@uqconnect.edu.au>
2016-05-20 22:23:31 +1000

    messed around with routes and stuff

---
+++ routes/BlackLion.js
@@ -0,0 +1,16 @@
+
+/*
+ * GET home page.
+ */
+
+exports.home = function(req, res){
+  res.render('home', {title: 'BLMA: Home'});
+};
+
+exports.TaiChi = function(req, res){
+  res.render('TaiChi', {title: 'BLMA: TaiChi'});
+};
+
+exports.KungFu = function(req, res){
+  res.render('KungFu', {title: 'BLMA: KungFu'});
+};
